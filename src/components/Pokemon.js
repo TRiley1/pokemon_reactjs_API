78a6be43f43c02524ffd7ce59c649fc929d64365
Thomas Riley <thomas@Thomass-MacBook-Pro.local>
2023-04-15 18:55:58 +0100

    toggle refactored

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -15,7 +15,7 @@ const Pokemon = ({pokemon}) => {
     <>
         <h3>{pokemon.name}</h3>
         <img src = {shiny? pokemon.sprites.front_shiny : pokemon.sprites.front_default } alt = {pokemon.name}/>
-        <button onClick = {handleClick} value = {pokemon.sprites.shiny_default}></button>
+        <button onClick = {handleClick}></button>
         
     </>
     );
